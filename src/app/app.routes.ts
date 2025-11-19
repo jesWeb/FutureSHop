a52c1aeb4241e3cdb78035dae6a8efebba4d2e00
jesus <jesus@2806-104e-0013-d90d-0000-0000-0000-0002.ipv6.infinitum.net.mx>
2025-11-19 12:21:34 -0600

    configuracion de rutas y arquitectura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  {
+    path: '',
+    loadChildren: () => import('./store-front/store-front.route').then(fro => fro.StoreFrontRoutes)
+  }
+
+
+
+];
