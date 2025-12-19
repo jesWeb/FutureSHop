dfd398bea352aed9db01c0524bd4668da723c2bd
cactusP <jg688085@gmail.com>
2025-12-18 18:01:30 -0600

    empezamos con el perfil de dashboard

---
+++ src/app/admin.dashboard/admin-dashboard.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const adminDashboardRoutes: Routes = [{}]
+
+
+export default adminDashboardRoutes;
---
+++ src/app/admin.dashboard/pages/producto-admin-layout/producto-admin-layout.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-admin-layout',
+  imports: [],
+  templateUrl: './producto-admin-layout.html',
+})
+export class ProductoAdminLayout { }
