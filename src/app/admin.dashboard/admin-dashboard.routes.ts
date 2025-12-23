13c21a915a6c9b75d4cd78653a41f13a08ed7943
cactusP <jg688085@gmail.com>
2025-12-23 17:56:58 -0600

    agregamos el diseno del dasboard

--- src/app/admin.dashboard/admin-dashboard.routes.ts
+++ src/app/admin.dashboard/admin-dashboard.routes.ts
@@ -1,6 +1,22 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardLayout } from './layouts/admin-dashboard-layout/admin-dashboard-layout';
+import { ProductoAdminLayout } from './pages/producto-admin-layout/producto-admin-layout';
 
-export const adminDashboardRoutes: Routes = [{}]
-
-
-export default adminDashboardRoutes;
+export const adminDashboardRoutes: Routes = [{
+  path: '',
+  component: AdminDashboardLayout,
+  children: [
+    {
+      path: 'products',
+      component: ProductoAdminLayout
+    },
+    {
+      path: 'products/:id',
+      component: ProductoAdminLayout
+    },
+    {
+      path: '**',
+      redirectTo:'products'
+    },
+  ]
+}]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,7 +19,11 @@ export const routes: Routes = [
       // }
     ]
   },
-
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin.dashboard/admin-dashboard.routes').then(dash=>dash.adminDashboardRoutes)
+  }
+  ,
 
   {
     path: '',
