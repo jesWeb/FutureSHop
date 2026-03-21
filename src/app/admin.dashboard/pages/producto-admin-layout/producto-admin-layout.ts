9525d1498579bfbce300a49d5efb1d5cdd67e0aa
Jesus Lopez <jeslgtz785@gmail.com>
2026-03-20 18:00:58 -0600

    m,ostrar productos paginados

--- src/app/admin.dashboard/pages/producto-admin-layout/producto-admin-layout.ts
+++ src/app/admin.dashboard/pages/producto-admin-layout/producto-admin-layout.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductTable } from "@app/products/components/Product-table/Product-table";
+import { ProductService } from '@app/products/services/products.services';
+import { PaginateService } from '@app/shared/components/pagination.service';
+import { Pagination } from "@app/shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-producto-admin-layout',
-  imports: [ProductTable],
+  imports: [ProductTable, Pagination],
   templateUrl: './producto-admin-layout.html',
 })
-export class ProductoAdminLayout { }
+export class ProductoAdminLayout {
+
+
+  productsService = inject(ProductService);
+  paginationServ = inject(PaginateService)
+
+
+  // rxResource
+  productsResource = rxResource({
+    params: () => ({ page: this.paginationServ.currentPage() - 1 }),
+    stream: ({ params }) => this.productsService.getProducts({
+      offset: params.page * 9,
+    })
+  });
+
+
+
+}
