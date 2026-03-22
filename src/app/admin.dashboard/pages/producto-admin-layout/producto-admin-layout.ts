6f4091c3e56ff48f57a3eb5c18ad73932c13dcd1
jesus <jesus@gmail.com>
2026-03-22 16:13:04 -0600

    termino de la paginacion de prodcutos con select

--- src/app/admin.dashboard/pages/producto-admin-layout/producto-admin-layout.ts
+++ src/app/admin.dashboard/pages/producto-admin-layout/producto-admin-layout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductTable } from "@app/products/components/Product-table/Product-table";
 import { ProductService } from '@app/products/services/products.services';
@@ -15,13 +15,17 @@ export class ProductoAdminLayout {
 
   productsService = inject(ProductService);
   paginationServ = inject(PaginateService)
+  productsPerPage = signal(10)
 
 
   // rxResource
   productsResource = rxResource({
-    params: () => ({ page: this.paginationServ.currentPage() - 1 }),
+    params: () => ({ page: this.paginationServ.currentPage() - 1,
+      limit: this.productsPerPage()
+    }),
     stream: ({ params }) => this.productsService.getProducts({
       offset: params.page * 9,
+      limit:params.limit
     })
   });
 
